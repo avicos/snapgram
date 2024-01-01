e3f3963c006ec1faf2198cda12a8cf1f1a5bfe62
avicos <adapaavinash612@gmail.com>
2024-01-01 16:57:07 +0530

    first commit

---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,22 @@
+import Bottombar from '@/components/shared/Bottombar';
+import LeftSideBar from '@/components/shared/LeftSideBar';
+import TopBar from '@/components/shared/TopBar';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+    return (
+        <div className='w-full md:flex'>
+            <TopBar />
+            <LeftSideBar />
+
+            <section className='flex flex-1 h-full'>
+                <Outlet />
+            </section>
+
+            <Bottombar />
+        </div>
+    );
+};
+
+export default RootLayout;
